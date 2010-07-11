9361bb4eb1748f7a91b50bc4334a54bb11a64582
Simon Kornblith <simon@simonster.com>
2010-07-11 10:10:23 +0000

    add license to new files

--- build/zoteroWinWordIntegration/zoteroException.h
+++ build/zoteroWinWordIntegration/zoteroException.h
@@ -1,3 +1,31 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+	
+	Copyright (c) 2009  Zotero
+	                    Center for History and New Media
+						George Mason University, Fairfax, Virginia, USA
+						http://zotero.org
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Permission is granted to link statically the libraries included with
+    a stock copy of Microsoft Windows. This library may not be linked, 
+    directly or indirectly, with any other proprietary code.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
 #pragma once
 #include "stdafx.h"
 #include "resource.h"
