1e34187b629eb40052264a330f61f2079984cbd0
Simon Kornblith <simon@simonster.com>
2014-04-21 02:25:14 -0400

    Clean up build
    Use Office 2010 in standard location, remove old targets and files

--- build/zoteroWinWordIntegration/CView0.h
+++ build/zoteroWinWordIntegration/CView0.h
@@ -1,7 +1,7 @@
 // Machine generated IDispatch wrapper class(es) created with Add Class from Typelib Wizard
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Office14\\MSO.DLL" auto_rename
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\VBA\\VBA6\\VBE6EXT.OLB" auto_rename
-#import "C:\\Program Files (x86)\\Microsoft Office 2010\\Office14\\MSWORD.OLB" auto_rename
+#import "C:\\Program Files (x86)\\Microsoft Office\\Office14\\MSWORD.OLB" auto_rename
 
 // CView0 wrapper class
 
