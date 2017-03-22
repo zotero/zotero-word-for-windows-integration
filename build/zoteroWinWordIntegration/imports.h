51f333a23de1b3e504615b1c49b870e3ac24c044
Adomas Venčkauskas <adomas.ven@gmail.com>
2017-03-22 17:14:04 +0200

    Upgrade project to Visual Studio 2015

--- build/zoteroWinWordIntegration/imports.h
+++ build/zoteroWinWordIntegration/imports.h
@@ -1,3 +1,3 @@
-#import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Office14\\MSO.DLL" auto_rename
+#import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Office15\\MSO.DLL" auto_rename
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\VBA\\VBA6\\VBE6EXT.OLB" auto_rename
-#import "C:\\Program Files (x86)\\Microsoft Office\\Office14\\MSWORD.OLB" auto_rename
+#import "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\MSWORD.OLB" auto_rename
