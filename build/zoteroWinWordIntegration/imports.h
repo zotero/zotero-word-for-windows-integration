zotero/zotero-word-for-windows-integration

commit 10005cc379c3b9ced77290cd23e78352571c5461

Fix Word 2013 crashing with many footnotes. Closes #40

build/zoteroWinWordIntegration/imports.h
@@ -1,3 +1,3 @@
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Office15\\MSO.DLL" auto_rename
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\VBA\\VBA6\\VBE6EXT.OLB" auto_rename
-#import "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\MSWORD.OLB" auto_rename
+#import "C:\\Program Files (x86)\\Microsoft Office 2007\\Office15\\MSWORD.OLB" auto_rename

commit 438e79309df9a970d291cd81a904691d800c7842

Fix crashing with Word 2016. Regression in 10005cc

build/zoteroWinWordIntegration/imports.h
@@ -1,3 +1,3 @@
-#import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Office15\\MSO.DLL" auto_rename
+#import "C:\\Program Files (x86)\\Microsoft Office\\root\\VFS\\ProgramFilesCommonX86\\Microsoft Shared\\OFFICE16\\MSO.DLL" auto_rename
 #import "C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\VBA\\VBA6\\VBE6EXT.OLB" auto_rename
-#import "C:\\Program Files (x86)\\Microsoft Office 2007\\Office15\\MSWORD.OLB" auto_rename
+#import "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\MSWORD.OLB" auto_rename