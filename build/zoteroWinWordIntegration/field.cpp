zotero/zotero-word-for-windows-integration

commit b65bfd9d96709dd5db4ee9c86906c6c8fa157207

Fix styling sometimes getting stuck with TNR. Closes #62

build/zoteroWinWordIntegration/field.cpp
@@ -278,10 +278,14 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 	HANDLE_EXCEPTIONS_BEGIN
 	setScreenUpdatingStatus(field->doc, false);
 	
-	// Get current font info
-	CFont0 comFont = field->comContentRange.get_Font();
-	CString fontName = comFont.get_Name();
-	float fontSize = comFont.get_Size();
+	// Get current style info
+	CString styleName;
+	VARIANT varStyle = field->comContentRange.get_Style();
+	if (varStyle.vt == VT_DISPATCH && varStyle.pdispVal != NULL) {
+		CStyle comStyle(varStyle.pdispVal);
+		styleName = comStyle.get_NameLocal();
+	}
+	VariantClear(&varStyle); // Clean up the VARIANT
 
 	// Check if we need to restore cursor position after insert (bookmarks only)
 	bool restoreSelectionToEnd = false;
@@ -344,8 +348,12 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 			}
 		}
 
-		// Put font back on
-		comFont = field->comContentRange.get_Font();
+		// We need to reset the font here, otherwise in mixed-font situations (e.g. when citing
+		// items that contain english and chinese characters) the font somehow goes to Times New Roman
+		CFont0 comFont = field->comContentRange.get_Font();
+		comFont.Reset();
+		// Put style back on
+		field->comContentRange.put_Style(styleName);
 
 		// Need to delete the return that gets added at the end, but only if there are no
 		// returns within the text to be inserted
@@ -373,6 +381,8 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 			field->comBookmark = comBookmarks.Add(field->bookmarkName, field->comContentRange);
 			field->comContentRange = field->comBookmark.get_Range();
 		}
+		// Put style back on
+		field->comContentRange.put_Style(styleName);
 	}
 
 	// Restore the selection to the end of a bookmark
@@ -382,8 +392,6 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 		comRange.Select();
 	}
 
-	comFont.put_Name(fontName);
-	comFont.put_Size(fontSize);
 	return STATUS_OK;
 	HANDLE_EXCEPTIONS_END
 }

commit 72e83647756b759ec80923922218fff8bf8b0343

Do not drop inline font when inserting rich text
Regression from b65bfd9

build/zoteroWinWordIntegration/field.cpp
@@ -278,21 +278,22 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 	HANDLE_EXCEPTIONS_BEGIN
 	setScreenUpdatingStatus(field->doc, false);
 	
-	// Get current font size
+	// Get current font size and name
 	CFont0 comFont = field->comContentRange.get_Font();
 	float fontSize = comFont.get_Size();
-	
-	// Get the font name from style instead of directly from the
-	// content range, because when there are mixed fonts (like when
-	// inserting text with mixed latin and chinese characters)
-	// the returned font style is an empty string "" and if you
-	// try to put that back on Word defaults to Times New Roman
-	CString fontName;
-	CComVariant varStyle = field->comContentRange.get_Style();
-	if (varStyle.vt == VT_DISPATCH && varStyle.pdispVal != NULL) {
-		CStyle comStyle(varStyle.pdispVal);
-		CFont0 styleFont = comStyle.get_Font();
-		fontName = styleFont.get_Name();
+	CString fontName = comFont.get_Name();
+
+	// If the font name from the range is empty (happens with mixed fonts,
+	// e.g. when inserting text with mixed latin and chinese characters),
+	// fall back to the font name from the current style. Otherwise Word
+	// defaults to Times New Roman when we put an empty font name back on.
+	if (fontName.IsEmpty()) {
+		CComVariant varStyle = field->comContentRange.get_Style();
+		if (varStyle.vt == VT_DISPATCH && varStyle.pdispVal != NULL) {
+			CStyle comStyle(varStyle.pdispVal);
+			CFont0 styleFont = comStyle.get_Font();
+			fontName = styleFont.get_Name();
+		}
 	}
 
 	// Check if we need to restore cursor position after insert (bookmarks only)
@@ -388,7 +389,7 @@ statusCode __stdcall setText(field_t* field, const wchar_t string[], bool isRich
 			field->comBookmark = comBookmarks.Add(field->bookmarkName, field->comContentRange);
 			field->comContentRange = field->comBookmark.get_Range();
 		}
-		// Put dont back on
+		// Put font back on
 		comFont.put_Name(fontName);
 		comFont.put_Size(fontSize);
 	}